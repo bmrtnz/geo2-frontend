b23419a5b048b8cd46023da8a646087601af66c7
PascalG <pascalgautier@microtec.fr>
2020-06-23 16:03:32 +0200

    Début modif headers colonnes datagrid + rectif accès contacts clients

--- src/app/pages/tiers/clients/details/client-details.component.ts
+++ src/app/pages/tiers/clients/details/client-details.component.ts
@@ -200,7 +200,7 @@ export class ClientDetailsComponent implements OnInit {
   }
 
   contactsBtnClick() {
-    this.router.navigate([`/tiers/contacts/${ this.client.id }/${ this.client.typeTiers }`]);
+    this.router.navigate([`/tiers/contacts/${ this.client.code }/${ this.client.typeTiers }`]);
   }
 
 }
