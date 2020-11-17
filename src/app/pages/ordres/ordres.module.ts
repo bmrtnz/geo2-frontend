d65fc23efca2d7c00ccba00adff8ae3baf409e88
PascalG <pascalgautier@microtec.fr>
2020-11-17 11:55:13 +0100

    Merge conflict ordres modules

--- src/app/pages/ordres/ordres.module.ts
+++ src/app/pages/ordres/ordres.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { OrdresAccueilComponent } from './accueil/ordres-accueil.component';
 import { OrdresDetailsComponent } from './details/ordres-details.component';
 import { OrdresIndicateursComponent } from './indicateurs/ordres-indicateurs.component';
 import { OrdresRoutingModule } from './ordres-routing.module';
-import { SharedModule } from 'app/shared/shared.module';
-=======
->>>>>>> 5c172372492ad4e4c6fe94b0a3a38c579e69a345
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditingAlertModule } from 'app/shared/components/editing-alert/editing-alert.component';
 import { FileManagerModule } from 'app/shared/components/file-manager/file-manager-popup.component';
@@ -24,18 +20,11 @@ import {
   DxTextAreaModule, DxTextBoxModule,
   DxTileViewModule, DxValidatorModule
 } from 'devextreme-angular';
-import { OrdresAccueilComponent } from './accueil/ordres-accueil.component';
-import { OrdresDetailsComponent } from './details/ordres-details.component';
 import { GridSuiviComponent } from './grid-suivi/grid-suivi.component';
-import { OrdresRoutingModule } from './ordres-routing.module';
 
 
 @NgModule({
-<<<<<<< HEAD
-  declarations: [OrdresAccueilComponent, OrdresDetailsComponent, OrdresIndicateursComponent],
-=======
-  declarations: [OrdresAccueilComponent, OrdresDetailsComponent, GridSuiviComponent],
->>>>>>> 5c172372492ad4e4c6fe94b0a3a38c579e69a345
+  declarations: [OrdresAccueilComponent, OrdresDetailsComponent, GridSuiviComponent, OrdresIndicateursComponent],
   imports: [
     OrdresRoutingModule,
     SharedModule,
