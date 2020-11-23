aaf6ea11d2c931ae8304518d2d6030095ece5aec
Thomas ROLLIN <thomas@microtec.fr>
2020-11-23 11:47:34 +0100

    fix clients global search

--- src/app/shared/models/base-tarif.model.ts
+++ src/app/shared/models/base-tarif.model.ts
@@ -3,7 +3,7 @@ import { Field, Model } from './model';
 export class BaseTarif extends Model {
   @Field({asKey: true}) public id: string;
   @Field({asLabel: true}) public description: string;
-  @Field() public cku: string;
+  @Field({allowSearch: false, allowHeaderFiltering: false}) public cku: string;
   @Field() public valideTrp: boolean;
   @Field() public valideLig: boolean;
   @Field() public valide: boolean;
