60e68d604394af0e2fabce9c1ce68a02022ad2ab
Thomas ROLLIN <thomas@microtec.fr>
2021-01-04 16:53:00 +0100

    resolve Raccordement table `GEO_MRU_ORDRE` #10437

---
+++ src/app/shared/services/api/mru-ordres.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { MRUOrdre } from 'app/shared/models/mru-ordre.model';
+import DataSource from 'devextreme/data/data_source';
+import { LoadOptions } from 'devextreme/data/load_options';
+import { APIRead, ApiService, RelayPage } from '../api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MruOrdresService extends ApiService implements APIRead {
+
+  constructor(
+    apollo: Apollo,
+  ) {
+    super(apollo, MRUOrdre);
+    this.gqlKeyType = 'GeoMRUOrdreKeyInput';
+  }
+
+  getDataSource() {
+    return new DataSource({
+      store: this.createCustomStore({
+        key: ['utilisateur', 'ordre'],
+        load: (options: LoadOptions) => new Promise(async (resolve) => {
+
+          if (options.group)
+            return this.loadDistinctQuery(options, res => {
+              if (res.data && res.data.distinct)
+                resolve(this.asListCount(res.data.distinct));
+            });
+
+          type Response = { allMRUOrdre: RelayPage<MRUOrdre> };
+          const query = await this.buildGetAll();
+          const variables = this.mapLoadOptionsToVariables(options);
+
+          this.listenQuery<Response>(query, { variables }, res => {
+            if (res.data && res.data.allMRUOrdre)
+              resolve(this.asInstancedListCount(res.data.allMRUOrdre));
+          });
+        }),
+        byKey: (key) => new Promise(async (resolve) => {
+          const query = await this.buildGetOne();
+          type Response = { MRUOrdre: MRUOrdre };
+          const id = key ? {
+            utilisateur: key.utilisateur || '',
+            espece: key.especeId || '',
+          } : {};
+          const variables = { id };
+          this.listenQuery<Response>(query, { variables }, res => {
+            if (res.data && res.data.MRUOrdre)
+              resolve(new this.model(res.data.MRUOrdre));
+          });
+        }),
+      }),
+    });
+  }
+
+}
