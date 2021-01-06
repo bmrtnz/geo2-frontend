3ed005d290276c5a306b1b5ae61a287d562b632b
Thomas ROLLIN <thomas@microtec.fr>
2021-01-06 11:43:11 +0100

    add articles e2e tests #10341

---
+++ e2e/src/articles.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { $, browser, by, element } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
+
+describe('Articles', () => {
+
+  it('should navigate to list', async () => {
+    await browser.get(`articles/list`);
+    await browser.wait(protractor.ExpectedConditions.visibilityOf($('.content dx-data-grid')), 10000);
+    const url = await browser.getCurrentUrl();
+    expect(url).toMatch(new RegExp(`articles\/list$`));
+  });
+
+  it('should display grid with rows', async () => {
+    expect($('.content dx-data-grid').isPresent()).toBeTruthy();
+    const firstRow = element.all(by.css('.content .dx-datagrid-rowsview tbody tr')).first();
+    await browser.wait(protractor.ExpectedConditions.visibilityOf(firstRow), 10000);
+  });
+
+  it('should navigate to form', async () => {
+    const firstRow = element.all(by.css('.content .dx-datagrid-rowsview tbody tr')).first();
+    firstRow.click();
+    firstRow.click();
+    await browser.wait(protractor.ExpectedConditions.visibilityOf($('.content form')), 10000);
+    const url = await browser.getCurrentUrl();
+    expect(url).toMatch(new RegExp(`articles\/(?!list)`));
+  });
+
+  it('should handle save with form', async () => {
+    await $('dx-button[aria-label="Éditer"]').click();
+    const textBox = $(`dx-text-box[formcontrolname="description"]`);
+    await textBox.element(by.css('input')).click();
+
+    await textBox.element(by.css('input')).clear();
+    await textBox.element(by.css('input')).sendKeys('[description]');
+
+    const submitBtn = $('dx-button[aria-label="Valider"]');
+    await submitBtn.click();
+    await browser.wait(protractor.ExpectedConditions.invisibilityOf(submitBtn), 10000);
+  });
+
+});
