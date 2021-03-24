401aa737b446260fa6055c51e8a5329265b2f8e1
Thomas ROLLIN <thomas@microtec.fr>
2021-03-24 11:56:06 +0100

    update MRU entrepots #11595

---
+++ src/app/shared/services/api/mru-entrepots.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { MruEntrepotsService } from './mru-entrepots.service';
+
+describe('MruEntrepotsService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: MruEntrepotsService = TestBed.get(MruEntrepotsService);
+    expect(service).toBeTruthy();
+  });
+});
