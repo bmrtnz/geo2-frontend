94dc2a1fa4158d7bd49d26988547455e35286d1b
Stephane Lamotte <stephane@blue-whale.com>
2021-05-11 13:54:51 +0200

    Configuration environnement de dev chez BW

---
+++ src/environments/environment.bwdev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  debugRouting: false,
+  apiEndpoint: 'http://geo2-test.bluewhale.fr/api',
+  columnChooser: {
+    width: 500,
+    height: 500
+  },
+  societe: {
+    id: '',
+    raisonSocial: ''
+  },
+};
