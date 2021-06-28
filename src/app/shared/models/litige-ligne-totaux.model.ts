9773e43fcff8b1713beac5455691657155d7ec71
Thomas ROLLIN <thomas@microtec.fr>
2021-06-28 14:44:28 +0200

    update ll-totaux GraphQL endpoint #11010

--- src/app/shared/models/litige-ligne-totaux.model.ts
+++ src/app/shared/models/litige-ligne-totaux.model.ts
@@ -4,11 +4,11 @@ import { Field, Model, ModelName } from './model';
 @ModelName('LitigeLigneTotaux')
 export class LitigeLigneTotaux extends Model {
 
-  @Field() public reclamationClientTaux: number;
-  @Field() public reclamationClient: number;
-  @Field() public deviseTotalTaux: number;
-  @Field() public deviseTotal: number;
-  @Field() public ristourne: number;
+  @Field() public avoirClient: number;
+  @Field() public avoirClientTaux: number;
+  @Field() public avoirFournisseur: number;
+  @Field() public avoirFournisseurTaux: number;
+  @Field() public ristourneTaux: number;
   @Field() public fraisAnnexes: number;
   @Field() public totalMontantRistourne: number;
   @Field({model: import('./devise.model')}) public devise: Devise;
