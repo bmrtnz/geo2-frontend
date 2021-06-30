9574e92604ca9d1a3aa0aa9fb53d528d185d44e5
PascalG <pascalgautier@microtec.fr>
2021-06-30 09:25:33 +0200

    Ajustement type tiers grid lignes ordres

---
+++ src/app/shared/models/type-tiers.model.ts
@@ -0,0 +1,15 @@
+import {Field, Model, ModelName} from './model';
+
+@ModelName('TypeTiers')
+export class TypeTiers extends Model {
+
+  @Field({asKey: true}) public id: string;
+  @Field({asLabel: true}) public description: string;
+  @Field() public responsableLitige: boolean;
+  @Field() public userModification: string;
+  @Field() public dateModification: Date;
+  @Field() public valide: boolean;
+  
+}
+
+export default TypeTiers;
