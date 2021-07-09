c58b921d32442028651396cda46356cbb3887bb5
Thomas ROLLIN <thomas@microtec.fr>
2021-07-09 11:25:58 +0200

    add tracabilité models #11010

---
+++ src/app/shared/models/tracabilite-ligne.model.ts
@@ -0,0 +1,16 @@
+import { Field, Model, ModelName } from './model';
+import OrdreLigne from './ordre-ligne.model';
+import TracabiliteDetailPalette from './tracabilite-detail-palette.model';
+
+@ModelName('TracabiliteLigne')
+export class TracabiliteLigne extends Model {
+
+  @Field({asKey: true}) public id?: number;
+  @Field() public arboCode?: string;
+  @Field() public nombreColis?: number;
+  @Field({model: import('./ordre-ligne.model')}) public ordreLigne?: OrdreLigne;
+  @Field({model: import('./tracabilite-detail-palette.model')}) public tracabiliteDetailPalette?: TracabiliteDetailPalette;
+
+}
+
+export default TracabiliteLigne;
