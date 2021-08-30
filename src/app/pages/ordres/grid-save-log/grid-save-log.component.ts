871981de66f6c1bf6b12dd610b2249833844b01f
Thomas ROLLIN <thomas@microtec.fr>
2021-08-30 14:51:58 +0200

    update ordre details grid-save-log #13793

--- src/app/pages/ordres/grid-save-log/grid-save-log.component.ts
+++ src/app/pages/ordres/grid-save-log/grid-save-log.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import type { Model } from 'app/shared/models/model';
 import { ModelFieldOptions } from 'app/shared/models/model';
 import Ordre from 'app/shared/models/ordre.model';
@@ -8,13 +8,14 @@ import { DxDataGridComponent } from 'devextreme-angular';
 import DataSource from 'devextreme/data/data_source';
 import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
+import { ToggledGrid } from '../details/ordres-details.component';
 
 @Component({
   selector: 'app-grid-save-log',
   templateUrl: './grid-save-log.component.html',
   styleUrls: ['./grid-save-log.component.scss'],
 })
-export class GridSaveLogComponent implements OnChanges {
+export class GridSaveLogComponent implements ToggledGrid {
   @Input() public ordre: Ordre;
   @ViewChild(DxDataGridComponent, { static: true })
   dataGrid: DxDataGridComponent;
@@ -29,19 +30,18 @@ export class GridSaveLogComponent implements OnChanges {
     private ordresSaveLogsService: OrdresSaveLogsService,
     public gridConfiguratorService: GridConfiguratorService
   ) {
-    this.dataSource = this.ordresSaveLogsService.getDataSource();
     this.detailedFields = this.ordresSaveLogsService.model
     .getDetailedFields(1, /^(?:utilisateur|dateModification)$/i, {forceFilter: true});
   }
 
-  ngOnChanges() {
-    this.enableFilters();
-  }
-
   enableFilters() {
     if (this.ordre) {
+      this.dataSource = this.ordresSaveLogsService.getDataSource();
       this.dataSource.filter([['ordre.id', '=', this.ordre.id]]);
-      this.dataSource.reload();
     }
+  }
+
+  onToggling(toggled: boolean) {
+    toggled ? this.enableFilters() : this.dataSource = null;
   }
 }
