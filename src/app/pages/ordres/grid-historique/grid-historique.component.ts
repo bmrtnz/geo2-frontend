7393474221793237e9e3285f4b028cabc93c5b87
Thomas ROLLIN <thomas@microtec.fr>
2021-09-01 10:40:16 +0200

    Update grid historique ordre #11595

--- src/app/pages/ordres/grid-historique/grid-historique.component.ts
+++ src/app/pages/ordres/grid-historique/grid-historique.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Model, ModelFieldOptions } from 'app/shared/models/model';
 import Ordre from 'app/shared/models/ordre.model';
+import { AuthService } from 'app/shared/services';
 import { MruOrdresService } from 'app/shared/services/api/mru-ordres.service';
-import { AuthService } from 'app/shared/services/auth.service';
 import { CurrentCompanyService } from 'app/shared/services/current-company.service';
 import { GridConfiguratorService } from 'app/shared/services/grid-configurator.service';
 import { LocalizationService } from 'app/shared/services/localization.service';
@@ -22,6 +22,8 @@ export class GridHistoriqueComponent implements OnInit {
   @Input() public filter: [];
   @ViewChild(DxDataGridComponent, {static :true}) dataGrid : DxDataGridComponent;
 
+  readonly INDICATOR_NAME = 'Historique';
+
   public dataSource: DataSource;
   public columnChooser = environment.columnChooser;
   public detailedFields: Observable<ModelFieldOptions<typeof Model> | ModelFieldOptions<typeof Model>[]>;
@@ -30,14 +32,14 @@ export class GridHistoriqueComponent implements OnInit {
   private gridFilter: RegExp = /^(?:ordre\.(numero|referenceClient|dateDepartPrevue|dateLivraisonPrevue|codeClient|codeAlphaEntrepot|dateModification|client\.raisonSocial|secteurCommercial\.id|entrepot\.raisonSocial))$/;
 
   constructor(
-    private ordresService: MruOrdresService,
+    private mruOrdresService: MruOrdresService,
     public currentCompanyService: CurrentCompanyService,
     private authService: AuthService,
     public localizeService: LocalizationService,
     public gridConfiguratorService: GridConfiguratorService,
   ) {
-    this.dataSource = ordresService.getDataSource(2, this.gridFilter);
-    this.detailedFields = this.ordresService.model
+    this.dataSource = mruOrdresService.getDataSource(2, this.gridFilter);
+    this.detailedFields = this.mruOrdresService.model
     .getDetailedFields(3, this.gridFilter, {forceFilter: true});
   }
 
