4dfd53030c27d7d6a3c7698fda39335c659eb7bf
Thomas ROLLIN <thomas@microtec.fr>
2021-10-29 16:57:44 +0200

    add form utils service #14421

---
+++ src/app/shared/services/form-utils.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormUtilsService {
+
+  constructor() { }
+
+  /**
+   * Filter dirty controls and map them to their field value
+   * @param controls Form controls
+   * @param entityKey Entity key field name
+   */
+  public extractDirty(
+    controls: { [key: string]: AbstractControl },
+    entityKey: string | string[],
+  ) {
+    const clean = (value) => {
+      if (value && value.__typename)
+        for (const field of Object.keys(value))
+          if (field !== entityKey)
+            delete value[field];
+      return value;
+    };
+    return Object.entries(controls)
+      .filter(([key, control]) => key === entityKey || control.dirty)
+      .map(([key, control]) => {
+        const value = JSON.parse(JSON.stringify(control.value));
+        const cleanValue = typeof value === 'object' && value && value.length !== undefined ?
+          (value as []).map(v => clean(v)) :
+          clean(value);
+        return { [key]: cleanValue };
+      })
+      .reduce((acm, current) => ({ ...acm, ...current }));
+  }
+
+  /**
+   * Map entity to field path list
+   * @param entity Entity data
+   * @param prefix Path prefix
+   */
+  public extractPaths(entity: {}, prefix = ''): string[] {
+    return Object
+    .entries(entity)
+    .flatMap(([key, value]) => {
+      if (typeof value === 'object') {
+        if (length in value)
+          return this.extractPaths(value[0], key);
+        return this.extractPaths(value, key);
+      }
+      return `${prefix ? `${prefix}.` : ''}${key}`;
+    });
+  }
+}
