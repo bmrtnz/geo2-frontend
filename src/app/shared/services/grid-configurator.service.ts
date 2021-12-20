765bacfc7563924cedf2d63abddef25d2babe4b7
PascalG <pascalgautier@microtec.fr>
2021-12-20 10:57:11 +0100

    Implémentation planning transporteurs (en cours)

--- src/app/shared/services/grid-configurator.service.ts
+++ src/app/shared/services/grid-configurator.service.ts
@@ -195,7 +195,7 @@ export class GridConfiguratorService {
       widget: 'dxTextBox',
       cssClass: 'grid-title',
       options: {
-        width: 400,
+        width: title.length ? 400 : 0,
         readOnly: true,
         text: title
       }
