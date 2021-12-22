0ca0c430280b33ac9d9d9e030410de56400bccfa
PascalG <pascalgautier@microtec.fr>
2021-12-22 10:08:31 +0100

    Petit correctif

--- src/app/pages/ordres/indicateurs/planning-fournisseurs/planning-fournisseurs.component.ts
+++ src/app/pages/ordres/indicateurs/planning-fournisseurs/planning-fournisseurs.component.ts
@@ -151,14 +151,14 @@ export class PlanningFournisseursComponent implements OnInit, AfterViewInit {
       }
       // Prix
       if (e.column.dataField === 'ventePrixUnitaire') {
-        if (!e.data.ventePrixUnitaire || !e.data.venteUnite.description) {
+        if (!e.data?.ventePrixUnitaire || !e.data?.venteUnite?.description) {
           e.cellElement.innerText = '';
         } else {
           e.cellElement.innerText =  e.cellElement.innerText + ' ' + e.data.ordre.devise.id + ' / ' + e.data.venteUnite.description;
         }
       }
       if (e.column.dataField === 'achatPrixUnitaire') {
-        if (!e.data.achatPrixUnitaire || !e.data.achatUnite.description) {
+        if (!e.data?.achatPrixUnitaire || !e.data?.achatUnite?.description) {
           e.cellElement.innerText = '';
         } else {
           e.cellElement.innerText =  e.cellElement.innerText + ' ' + e.data.ordre.devise.id + ' / ' + e.data.achatUnite.description;
