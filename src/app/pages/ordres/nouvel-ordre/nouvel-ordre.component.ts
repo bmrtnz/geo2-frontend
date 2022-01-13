32867d960c73334b8505163420d02b1d5166ba7f
Thomas <thomas@microtec.fr>
2022-01-13 15:17:31 +0100

    ondoing ordre-generation #15071

--- src/app/pages/ordres/nouvel-ordre/nouvel-ordre.component.ts
+++ src/app/pages/ordres/nouvel-ordre/nouvel-ordre.component.ts
@@ -1,14 +1,17 @@
 import { Component, ViewChild } from '@angular/core';
-import { ApolloQueryResult } from '@apollo/client/core';
 import { Entrepot } from 'app/shared/models';
-import { FunctionsService, Response as FunctionResponse } from 'app/shared/services/api/functions.service';
+import MRUEntrepot from 'app/shared/models/mru-entrepot.model';
+import { FunctionsService } from 'app/shared/services/api/functions.service';
+import { OrdresService } from 'app/shared/services/api/ordres.service';
+import { CurrentCompanyService } from 'app/shared/services/current-company.service';
 import { SingleSelection } from 'basic';
+import { DxPopupComponent } from 'devextreme-angular';
 import notify from 'devextreme/ui/notify';
-import { Observable } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
+import { defer, Observable } from 'rxjs';
+import { concatMapTo, map, takeUntil, tap } from 'rxjs/operators';
 import { GridEntrepotsComponent } from '../grid-entrepots/grid-entrepots.component';
 import { GridHistoriqueEntrepotsComponent } from '../grid-historique-entrepots/grid-historique-entrepots.component';
-import MRUEntrepot from 'app/shared/models/mru-entrepot.model';
+import { TabContext } from '../root/root.component';
 
 @Component({
   selector: 'app-nouvel-ordre',
@@ -19,30 +22,42 @@ export class NouvelOrdreComponent {
 
   public typeEntrepots = ['Favoris', 'Tous'];
   public favorites = true;
-  public resolver: Observable<ApolloQueryResult<FunctionResponse>>;
+  public resolver: Observable<string>;
+
+  private ofValideEntrepotForOrdreRef = defer(() => this.functionsService
+  .ofValideEntrepotForOrdre(this.getSelectedEntrepot().id).valueChanges);
+  private fNouvelOrdreRef = defer(() => this.functionsService
+  .fNouvelOrdre(this.currentCompanyService.getCompany().id).valueChanges);
 
   @ViewChild(GridEntrepotsComponent, { static: false })
   EntrepotGrid: GridEntrepotsComponent;
   @ViewChild(GridHistoriqueEntrepotsComponent, { static: false })
   historiqueEntrepotGrid: GridHistoriqueEntrepotsComponent;
   @ViewChild('grid') private grid: SingleSelection<Entrepot|MRUEntrepot>;
+  @ViewChild(DxPopupComponent) private popup: DxPopupComponent;
 
   constructor(
     private functionsService: FunctionsService,
+    private ordresService: OrdresService,
+    private currentCompanyService: CurrentCompanyService,
+    private tabContext: TabContext,
   ) { }
 
   onButtonLoaderClick() {
 
-    this.resolver = this.functionsService
-    .ofValideEntrepotForOrdre(this.getSelectedEntrepot().id)
-    .valueChanges
+    this.resolver = this.ofValideEntrepotForOrdreRef
     .pipe(
-      takeWhile( res => res.loading ),
+      concatMapTo(this.fNouvelOrdreRef),
+      tap( res => this.popup.visible = true ),
+      takeUntil(this.popup.onHiding),
+      map( res => res.data.fNouvelOrdre.data.ls_nordre ),
     );
 
-    this.resolver.subscribe({
+    this.resolver
+    // .pipe(takeUntil(this.popup.onHiding))
+    .subscribe({
+      // next: numero => this.tabContext.openOrdre(numero),
       error: err => notify(err.message, 'warning', 3000),
-      complete: () => console.log('DONE'),
     });
 
   }
@@ -56,5 +71,4 @@ export class NouvelOrdreComponent {
   onTypeChange(e) {
     this.favorites = (e.value === this.typeEntrepots[0]);
   }
-
 }
