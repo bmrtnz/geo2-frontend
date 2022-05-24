b0679761695977f92678a2fc4130e070d76e15ac
PascalG <pascalgautier@microtec.fr>
2022-05-24 16:09:58 +0200

    #16647 : ajust condition possibilité modifier

--- src/app/pages/ordres/form/form.component.ts
+++ src/app/pages/ordres/form/form.component.ts
@@ -116,7 +116,9 @@ export class FormComponent implements OnInit, OnDestroy, AfterViewInit {
         "hasLitige",
         "cqLignesCount",
         "commentairesOrdreCount",
-        "regimeTva.id"
+        "regimeTva.id",
+        "facture",
+        "bonAFacturer"
     ];
 
     private destroy = new Subject<boolean>();
