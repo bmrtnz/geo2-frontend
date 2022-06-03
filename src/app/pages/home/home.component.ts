8ad996e583c5f8c36882180b8b145dfdc6ec17b2
PascalG <pascalgautier@microtec.fr>
2022-06-03 10:09:40 +0200

    #16994  Accueil - Correctifs liens des boutons "modules"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,44 +4,44 @@ import { Router } from "@angular/router";
 import { DxDrawerComponent } from "devextreme-angular";
 
 @Component({
-    templateUrl: "home.component.html",
-    styleUrls: ["./home.component.scss"],
-    providers: [NavHomeService],
+  templateUrl: "home.component.html",
+  styleUrls: ["./home.component.scss"],
+  providers: [NavHomeService],
 })
 export class HomeComponent implements OnInit {
-    @ViewChild(DxDrawerComponent, { static: false }) drawer: DxDrawerComponent;
-    public currentDate: Date;
-    public navigation: NavHomeList[];
-    public prod = !isDevMode();
+  @ViewChild(DxDrawerComponent, { static: false }) drawer: DxDrawerComponent;
+  public currentDate: Date;
+  public navigation: NavHomeList[];
+  public prod = !isDevMode();
 
-    constructor(private router: Router, service: NavHomeService) {
-        this.currentDate = new Date();
-        this.navigation = service.getNavigationList();
+  constructor(private router: Router, service: NavHomeService) {
+    this.currentDate = new Date();
+    this.navigation = service.getNavigationList();
+  }
+
+  ngOnInit() { }
+
+  onClickMultipleMenu(e) {
+    this.drawer.instance.toggle();
+  }
+
+  onClickItem(e) {
+    const target = e.element.id.replace("-button", "");
+    this.router.navigate([`/pages/stock`]);
+  }
+
+  navigate(view, nested?) {
+    console.log(view);
+    if (nested) {
+      this.router.navigateByUrl("/pages/nested/n/(" + view + ")");
+    } else {
+      this.router.navigateByUrl(view);
     }
+  }
 
-    ngOnInit() { }
-
-    onClickMultipleMenu(e) {
-        this.drawer.instance.toggle();
-    }
-
-    onClickItem(e) {
-        const target = e.element.id.replace("-button", "");
-        this.router.navigate([`/pages/stock`]);
-    }
-
-    navigate(view, nested?) {
-        if (nested) {
-            console.log(view);
-            this.router.navigateByUrl("/pages/nested/n/(" + view + ")");
-        } else {
-            this.router.navigateByUrl(view);
-        }
-    }
-
-    navigateItemList(e) {
-        this.router.navigateByUrl(
-            "/pages/nested/n/(" + e.itemData.path + "/list)",
-        );
-    }
+  navigateItemList(e) {
+    this.router.navigateByUrl(
+      "/pages/nested/n/(" + e.itemData.path + "/list)",
+    );
+  }
 }
