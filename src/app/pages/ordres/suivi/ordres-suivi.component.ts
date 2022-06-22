dc17ba91b62491cbb7a1dfe2bfb93e1afa4c5e15
PascalG <pascalgautier@microtec.fr>
2022-06-22 16:38:10 +0200

    #17171 Rech Ordres : filtrer client sur code, et non raison sociale

--- src/app/pages/ordres/suivi/ordres-suivi.component.ts
+++ src/app/pages/ordres/suivi/ordres-suivi.component.ts
@@ -1,18 +1,18 @@
 import {
-    AfterViewInit,
-    Component,
-    EventEmitter,
-    ViewChild,
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  ViewChild,
 } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { PushHistoryPopupComponent } from "app/shared/components/push-history-popup/push-history-popup.component";
 import { LocalizationService } from "app/shared/services";
 import { CurrentCompanyService } from "app/shared/services/current-company.service";
 import {
-    DxAutocompleteComponent,
-    DxPopupComponent,
-    DxSelectBoxComponent,
-    DxValidationGroupComponent,
+  DxAutocompleteComponent,
+  DxPopupComponent,
+  DxSelectBoxComponent,
+  DxValidationGroupComponent,
 } from "devextreme-angular";
 import DataSource from "devextreme/data/data_source";
 import { filter } from "rxjs/operators";
@@ -24,137 +24,137 @@ import { CampagnesService } from "app/shared/services/api/campagnes.service";
 let self;
 
 @Component({
-    selector: "app-ordres-suivi",
-    templateUrl: "./ordres-suivi.component.html",
-    styleUrls: ["./ordres-suivi.component.scss"],
+  selector: "app-ordres-suivi",
+  templateUrl: "./ordres-suivi.component.html",
+  styleUrls: ["./ordres-suivi.component.scss"],
 })
 export class OrdresSuiviComponent implements AfterViewInit {
-    readonly INDICATOR_ID = "SuiviDesOrdres";
+  readonly INDICATOR_ID = "SuiviDesOrdres";
 
-    searchItems: any;
-    filter: any;
-    campagnes: DataSource;
-    campagneEnCours: any;
-    showGridResults = false;
-    @ViewChild(DxAutocompleteComponent, { static: false })
-    autocomplete: DxAutocompleteComponent;
-    validatePopup: PushHistoryPopupComponent;
-    ordresLignesViewExp: boolean;
+  searchItems: any;
+  filter: any;
+  campagnes: DataSource;
+  campagneEnCours: any;
+  showGridResults = false;
+  @ViewChild(DxAutocompleteComponent, { static: false })
+  autocomplete: DxAutocompleteComponent;
+  validatePopup: PushHistoryPopupComponent;
+  ordresLignesViewExp: boolean;
 
-    refreshGrid = new EventEmitter();
+  refreshGrid = new EventEmitter();
 
-    @ViewChild(GridSuiviComponent, { static: false })
-    suiviGrid: GridSuiviComponent;
-    @ViewChild(GridHistoriqueComponent, { static: false })
-    histoGrid: GridHistoriqueComponent;
-    @ViewChild("searchCriteria", { static: false })
-    searchCriteria: DxSelectBoxComponent;
-    @ViewChild("currCampaign", { static: false })
-    currCampaign: DxSelectBoxComponent;
+  @ViewChild(GridSuiviComponent, { static: false })
+  suiviGrid: GridSuiviComponent;
+  @ViewChild(GridHistoriqueComponent, { static: false })
+  histoGrid: GridHistoriqueComponent;
+  @ViewChild("searchCriteria", { static: false })
+  searchCriteria: DxSelectBoxComponent;
+  @ViewChild("currCampaign", { static: false })
+  currCampaign: DxSelectBoxComponent;
 
-    constructor(
-        public localizeService: LocalizationService,
-        public currentCompanyService: CurrentCompanyService,
-        public campagnesService: CampagnesService,
-        public tabContext: TabContext,
-        private route: ActivatedRoute,
-    ) {
-        self = this;
-        this.searchItems = [
-            "numero",
-            "numeroFacture",
-            "referenceClient",
-            "client.raisonSocial",
-            "id",
-        ];
-        this.campagnesService
-            .getDataSource_v2(["id", "description"])
-            .load()
-            .then((camp) => (this.campagneEnCours = camp.slice(-1)[0]));
+  constructor(
+    public localizeService: LocalizationService,
+    public currentCompanyService: CurrentCompanyService,
+    public campagnesService: CampagnesService,
+    public tabContext: TabContext,
+    private route: ActivatedRoute,
+  ) {
+    self = this;
+    this.searchItems = [
+      "numero",
+      "numeroFacture",
+      "referenceClient",
+      "client.code",
+      "id",
+    ];
+    this.campagnesService
+      .getDataSource_v2(["id", "description"])
+      .load()
+      .then((camp) => (this.campagneEnCours = camp.slice(-1)[0]));
+  }
+
+  ngAfterViewInit() {
+    this.route.paramMap
+      .pipe(
+        filter(
+          (param) =>
+            param.get(RouteParam.TabID) === this.INDICATOR_ID,
+        ),
+      )
+      .subscribe((_) => {
+        this.histoGrid.reload();
+        if (this.suiviGrid) this.suiviGrid.reload();
+      });
+    this.resetCriteria();
+  }
+
+  searchDisplayExpr(item) {
+    return item
+      ? self.localizeService.localize(
+        "rechOrdres-" + item.split(".").join("-"),
+      )
+      : null;
+  }
+
+  resetCriteria() {
+    this.searchCriteria.instance.option("value", this.searchItems[0]);
+  }
+
+  changeSearchCriteria() {
+    const toSearch = this.autocomplete.value;
+    this.showGridResults = false;
+    if (toSearch) {
+      setTimeout(() => {
+        this.enableFilters(toSearch);
+        this.showGridResults = true;
+      }, 100);
     }
+  }
 
-    ngAfterViewInit() {
-        this.route.paramMap
-            .pipe(
-                filter(
-                    (param) =>
-                        param.get(RouteParam.TabID) === this.INDICATOR_ID,
-                ),
-            )
-            .subscribe((_) => {
-                this.histoGrid.reload();
-                if (this.suiviGrid) this.suiviGrid.reload();
-            });
-        this.resetCriteria();
+  changeCampaign() {
+    const toSearch = this.autocomplete.value;
+    if (toSearch?.length) {
+      this.showGridResults = false;
+      this.findOrder();
     }
+  }
 
-    searchDisplayExpr(item) {
-        return item
-            ? self.localizeService.localize(
-                "rechOrdres-" + item.split(".").join("-"),
-            )
-            : null;
+  enableFilters(value) {
+    if (!value?.length) return;
+    const criteria = this.searchCriteria.instance.option("value");
+    const operator = ["numero", "numeroFacture", "id"].includes(criteria)
+      ? "="
+      : "contains";
+
+    this.filter = [
+      ["valide", "=", true],
+      "and",
+      ["societe.id", "=", this.currentCompanyService.getCompany().id],
+      // 'and',
+      // ['facture', '=', false],
+      "and",
+      [criteria, operator, value],
+    ];
+
+    // Current campaing filtering
+    if (this.currCampaign.instance.option("value")) {
+      this.filter.push("and", [
+        "campagne.id",
+        "=",
+        this.campagneEnCours.id,
+      ]);
     }
+  }
 
-    resetCriteria() {
-        this.searchCriteria.instance.option("value", this.searchItems[0]);
-    }
-
-    changeSearchCriteria() {
-        const toSearch = this.autocomplete.value;
-        this.showGridResults = false;
-        if (toSearch) {
-            setTimeout(() => {
-                this.enableFilters(toSearch);
-                this.showGridResults = true;
-            }, 100);
-        }
-    }
-
-    changeCampaign() {
-        const toSearch = this.autocomplete.value;
-        if (toSearch?.length) {
-            this.showGridResults = false;
-            this.findOrder();
-        }
-    }
-
-    enableFilters(value) {
-        if (!value?.length) return;
-        const criteria = this.searchCriteria.instance.option("value");
-        const operator = ["numero", "numeroFacture", "id"].includes(criteria)
-            ? "="
-            : "contains";
-
-        this.filter = [
-            ["valide", "=", true],
-            "and",
-            ["societe.id", "=", this.currentCompanyService.getCompany().id],
-            // 'and',
-            // ['facture', '=', false],
-            "and",
-            [criteria, operator, value],
-        ];
-
-        // Current campaing filtering
-        if (this.currCampaign.instance.option("value")) {
-            this.filter.push("and", [
-                "campagne.id",
-                "=",
-                this.campagneEnCours.id,
-            ]);
-        }
-    }
-
-    findOrder() {
-        setTimeout(() => {
-            const toSearch = this.autocomplete.value;
-            if (toSearch?.length) {
-                this.enableFilters(toSearch);
-                this.showGridResults = true;
-            }
-        }, 1);
-    }
+  findOrder() {
+    setTimeout(() => {
+      const toSearch = this.autocomplete.value;
+      if (toSearch?.length) {
+        this.enableFilters(toSearch);
+        this.showGridResults = true;
+      }
+    }, 1);
+  }
 }
 
 export default OrdresSuiviComponent;
