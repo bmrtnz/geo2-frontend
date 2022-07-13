5f99fee5741135c5a3a63dafe26c38a682171719
Thomas <thomas@microtec.fr>
2022-07-13 16:16:32 +0200

    add `StockMouvement` save endpoint #17180

--- src/app/shared/services/api/stock-mouvements.service.ts
+++ src/app/shared/services/api/stock-mouvements.service.ts
@@ -12,8 +12,18 @@ export class StockMouvementsService extends ApiService {
     super(apollo, StockMouvement);
   }
 
+  public saveStockMouvement(
+    stockMouvement: Partial<StockMouvement>,
+    body: Set<string>,
+  ) {
+    return this.apollo.mutate<{ saveStockMouvement: Partial<StockMouvement> }>({
+      mutation: gql(this.buildSaveGraph([...body])),
+      variables: { stockMouvement },
+    });
+  }
+
   public deleteStockMouvement(id: string) {
-    return this.apollo.mutate({
+    return this.apollo.mutate<{ deleteStockMouvement: boolean }>({
       mutation: gql(this.buildDeleteGraph()),
       variables: { id },
     });
