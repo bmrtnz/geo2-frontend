d0e1a54af731a4cf94cfbf67e8b9371ab1e6cd28
Thomas <thomas@microtec.fr>
2022-07-18 14:50:31 +0200

    add `fResaUneLigne` GQL startpoint #17180

--- src/app/shared/services/api/stock-mouvements.service.ts
+++ src/app/shared/services/api/stock-mouvements.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Apollo, gql } from "apollo-angular";
 import StockMouvement from "app/shared/models/stock-mouvement.model";
 import { ApiService } from "../api.service";
+import { functionBody, FunctionResponse } from "./functions.service";
 
 @Injectable({
   providedIn: "root"
@@ -28,4 +29,62 @@ export class StockMouvementsService extends ApiService {
       variables: { id },
     });
   }
+
+  public fResaUneLigne =
+    (
+      fouCode: string,
+      propCode: string,
+      artRef: string,
+      username: string,
+      qteResa: number,
+      ordRef: string,
+      orlRef: string,
+      desc: string,
+      palCode: string,
+    ) => this.apollo
+      .watchQuery<{ fResaUneLigne: FunctionResponse }>({
+        query: gql(ApiService.buildGraph(
+          "query",
+          [
+            {
+              name: "fResaUneLigne",
+              body: functionBody,
+              params: [
+                { name: "fouCode", value: "fouCode", isVariable: true },
+                { name: "propCode", value: "propCode", isVariable: true },
+                { name: "artRef", value: "artRef", isVariable: true },
+                { name: "username", value: "username", isVariable: true },
+                { name: "qteResa", value: "qteResa", isVariable: true },
+                { name: "ordRef", value: "ordRef", isVariable: true },
+                { name: "orlRef", value: "orlRef", isVariable: true },
+                { name: "desc", value: "desc", isVariable: true },
+                { name: "palCode", value: "palCode", isVariable: true },
+              ]
+            }
+          ],
+          [
+            { name: "fouCode", type: "String", isOptionnal: false },
+            { name: "propCode", type: "String", isOptionnal: false },
+            { name: "artRef", type: "String", isOptionnal: false },
+            { name: "username", type: "String", isOptionnal: false },
+            { name: "qteResa", type: "String", isOptionnal: false },
+            { name: "ordRef", type: "String", isOptionnal: false },
+            { name: "orlRef", type: "String", isOptionnal: false },
+            { name: "desc", type: "String", isOptionnal: false },
+            { name: "palCode", type: "String", isOptionnal: false },
+          ],
+        )),
+        variables: {
+          fouCode,
+          propCode,
+          artRef,
+          username,
+          qteResa,
+          ordRef,
+          orlRef,
+          desc,
+          palCode,
+        },
+        fetchPolicy: "network-only",
+      })
 }
