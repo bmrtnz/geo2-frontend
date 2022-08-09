0e3b50762bb0536832c0f1634f97c569c4e75742
Thomas <thomas@microtec.fr>
2022-08-09 10:04:07 +0200

    add `entrepot-transporteur-bassin` model #17551

---
+++ src/app/shared/models/entrepot-transporteur-bassin.model.ts
@@ -0,0 +1,19 @@
+import BaseTarif from "./base-tarif.model";
+import BureauAchat from "./bureau-achat.model";
+import Devise from "./devise.model";
+import Entrepot from "./entrepot.model";
+import { Field, Model, ModelName } from "./model";
+import Transporteur from "./transporteur.model";
+
+@ModelName("EntrepotTransporteurBassin")
+export class EntrepotTransporteurBassin extends Model {
+  @Field({ asKey: true }) public id: number;
+  @Field({ model: import("./entrepot.model") }) public entrepot: Entrepot;
+  @Field({ model: import("./transporteur.model") }) public transporteur: Transporteur;
+  @Field({ model: import("./bureau-achat.model") }) public bureauAchat: BureauAchat;
+  @Field({ model: import("./base-tarif.model") }) public baseTarifTransport: BaseTarif;
+  @Field({ model: import("./devise.model") }) public DeviseTarifTransport: Devise;
+  @Field() public prixUnitaireTarifTransport: number;
+}
+
+export default EntrepotTransporteurBassin;
