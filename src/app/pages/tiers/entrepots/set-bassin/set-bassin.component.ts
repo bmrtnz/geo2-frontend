3adab82ba69d4fce5241fd627af6165990ec4da8
Thomas <thomas@microtec.fr>
2022-08-25 14:18:35 +0200

    fix `transporteurs-bassins` transporteur column #17551

--- src/app/pages/tiers/entrepots/set-bassin/set-bassin.component.ts
+++ src/app/pages/tiers/entrepots/set-bassin/set-bassin.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Entrepot, Transporteur } from "app/shared/models";
 import { TransporteursService } from "app/shared/services";
-import { BureauxAchatService } from "app/shared/services/api/bureaux-achat.service";
 import { EntrepotsTransporteursBassinsService } from "app/shared/services/api/entrepots-transporteurs-bassins.service";
 import { Grid, GridConfiguratorService } from "app/shared/services/grid-configurator.service";
 import { GridColumn } from "basic";
-import { DxDataGridComponent } from "devextreme-angular";
 import DataSource from "devextreme/data/data_source";
-import dxDataGrid from "devextreme/ui/data_grid";
 import { Observable } from "rxjs";
 import { concatMap, map, tap } from "rxjs/operators";
 
@@ -41,16 +38,6 @@ export class SetBassinComponent implements OnInit {
     this.transporteurs = this.transporteursService
       .getList("valide==true", ["id", "raisonSocial"])
       .pipe(map(res => res.data.allTransporteurList));
-  }
-
-  // public bureauGridSelectionChanged(event: { component: dxDataGrid, currentSelectedRowKeys: any[] }) {
-  //   event.component.collapseAll(-1);
-  //   event.component.expandRow(event.currentSelectedRowKeys[0]);
-  // }
-
-  public setStateValue(rowData: any, value: any): void {
-    console.log(rowData, value);
-    (this as any).defaultSetCellValue(rowData, value);
   }
 
   private fetchDatasource(entrepotID: Entrepot["id"]) {
