5a9fab1f38a2ca0c7262a9c65edabe289ae56ef8
Thomas <thomas@microtec.fr>
2022-09-15 15:08:49 +0200

    update `GeoGridConfig` entity structure #16112

--- src/app/shared/models/grid-config.model.ts
+++ src/app/shared/models/grid-config.model.ts
@@ -1,12 +1,15 @@
 import { Field, Model, ModelName } from "./model";
+import Societe from "./societe.model";
 import { Utilisateur } from "./utilisateur.model";
 
 @ModelName("GridConfig")
 export class GridConfig extends Model {
-    @Field({ asKey: true, model: import("./utilisateur.model") })
-    public utilisateur: Utilisateur;
-    @Field({ asKey: true, asLabel: true }) public grid: string;
-    @Field() public config: any;
+  @Field({ asKey: true, model: import("./utilisateur.model") })
+  public utilisateur: Utilisateur;
+  @Field({ model: import("./societe.model") })
+  public societe: Societe;
+  @Field({ asKey: true, asLabel: true }) public grid: string;
+  @Field() public config: any;
 }
 
 export default GridConfig;
