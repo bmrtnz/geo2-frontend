1f9e3152241e80ac0dad12b2b4783203624c3878
Thomas <thomas@microtec.fr>
2022-09-19 14:49:25 +0200

    add specific version label for development

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,20 +1,21 @@
 import { Component } from "@angular/core";
-import { version } from "../../../package.json";
+import { VersionService } from "app/shared/services/version.service";
 
 @Component({
-    selector: "app-pages",
-    templateUrl: "./pages.component.html",
-    styleUrls: ["./pages.component.scss"],
+  selector: "app-pages",
+  templateUrl: "./pages.component.html",
+  styleUrls: ["./pages.component.scss"],
 })
 export class PagesComponent {
-    public static readonly START_DEV_YEAR: number = 2020;
-    public readonly version = version;
-    public copyrightYear = "";
+  public static readonly START_DEV_YEAR: number = 2020;
+  public copyrightYear = "";
 
-    constructor() {
-        const year = new Date().getFullYear();
-        if (year !== PagesComponent.START_DEV_YEAR) {
-            this.copyrightYear = "-" + year;
-        }
+  constructor(
+    public versionService: VersionService,
+  ) {
+    const year = new Date().getFullYear();
+    if (year !== PagesComponent.START_DEV_YEAR) {
+      this.copyrightYear = "-" + year;
     }
+  }
 }
---
+++ src/app/shared/services/version.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { environment } from "environments/environment";
+import { version } from "../../../../package.json";
+
+@Injectable({
+  providedIn: "root"
+})
+export class VersionService {
+  getLabel() {
+    return environment.production ? `v ${version}` : "version de développement";
+  }
+}
