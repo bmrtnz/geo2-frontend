983ef8d53230f3dc9fbf346aefefe767015d03af
Thomas <thomas@microtec.fr>
2022-11-28 23:38:37 +0100

    add program download action

--- src/app/shared/services/program.service.ts
+++ src/app/shared/services/program.service.ts
@@ -19,14 +19,26 @@ export class ProgramService {
   /** Name of the body parameter in the backend */
   public static bodyName = "chunk";
 
-  /** Build ther URL that match the program */
-  public static buildUrl(program: Program): string {
+  /** Build the import URL that match the program */
+  public static buildImportUrl(program: Program): string {
     return `${environment.apiEndpoint}/program/${program.toString()}`;
   }
 
   /** Build the `accept` header */
   public static buildAccept(): string {
     return SUPPORTED_MIMES.join();
+  }
+
+  /** Build the download URL */
+  public static buildDownloadUrl(): string {
+    return `${environment.apiEndpoint}/program/download`;
+  }
+
+  /** Try downloadind the last processed program with browser save dialog */
+  public static downloadAndSave() {
+    const aElm = document.createElement("a");
+    aElm.href = ProgramService.buildDownloadUrl();
+    aElm.click();
   }
 
 }
