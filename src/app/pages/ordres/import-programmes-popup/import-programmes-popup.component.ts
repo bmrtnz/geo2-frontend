6360e5ddd5ac1d3355f6f16933dd7d20bcfeca55
PascalG <pascalgautier@microtec.fr>
2022-12-01 10:51:50 +0100

    #18635 programmes : ajout popup choix entrepotBWUK TESCO et ajustements front

--- src/app/pages/ordres/import-programmes-popup/import-programmes-popup.component.ts
+++ src/app/pages/ordres/import-programmes-popup/import-programmes-popup.component.ts
@@ -26,6 +26,7 @@ export class ImportProgrammesPopupComponent implements OnChanges {
   public buildAccept = ProgramService.buildAccept();
   public programService = ProgramService;
   public loading: boolean;
+  public entrepotBWUK: boolean;
 
   @ViewChild(GridImportProgrammesComponent, { static: false }) gridComponent: GridImportProgrammesComponent;
   @ViewChild(DxPopupComponent, { static: false }) popup: DxPopupComponent;
@@ -42,20 +43,47 @@ export class ImportProgrammesPopupComponent implements OnChanges {
     this.setTitle();
   }
 
-  onBeforeSend(e) {
-    this.gridComponent.datagrid.dataSource = null;
-    this.loading = true;
-    e.request.withCredentials = true;
-    e.request.responseType = "json";
-    this.gridComponent.datagrid.instance.beginCustomLoading("");
-  }
-
   uploadUrl() {
     return this.programService.buildImportUrl(Program[this.program.name]);
   }
 
+  onClickUpload() {
+    const chooseFileButton = document.querySelector(".import-programme-popup .dx-fileuploader-button") as HTMLElement;
+    if (this.programID === "tesco") {
+      confirm(
+        this.localizeService.localize("entrepot-import-programme"),
+        this.title
+      ).then(ent => {
+        this.entrepotBWUK = ent;
+        chooseFileButton.click();
+      });
+    } else {
+      chooseFileButton.click();
+    }
+  }
+
+  onBeforeSend(e) {
+    this.loading = true;
+    this.gridComponent.datagrid.dataSource = null;
+
+    // Use this.entrepotBWUK when TESCO
+    e.request.withCredentials = true;
+    e.request.responseType = "json";
+  }
+
+  onProgress(e) {
+    const progressStatut = document.querySelector(".import-programme-popup .dx-progressbar-status") as HTMLElement;
+    if (progressStatut?.innerHTML === "100%") {
+      progressStatut.innerHTML = this.localizeService.localize("traitement-en-cours");
+      this.gridComponent.datagrid.instance.beginCustomLoading("");
+    }
+  }
+
   onUploaded(e) {
     const DSitems = e.request?.response?.rows;
+
+    if (!DSitems?.length) return this.noDataError();
+
     DSitems.map((item, index) => {
       item.id = index;
       item.erreurs = item.erreurs.join(" / ");
@@ -65,7 +93,7 @@ export class ImportProgrammesPopupComponent implements OnChanges {
     this.gridComponent.datagrid.dataSource = DSitems;
 
     // Downloading modified Excel file
-    this.programService.downloadAndSave();
+    // this.programService.downloadAndSave();
     notify(
       this.localizeService.localize("telechargement-fichier").replace("&F", e.file.name),
       "success",
@@ -83,6 +111,11 @@ export class ImportProgrammesPopupComponent implements OnChanges {
     this.endLoading();
     notify(`${e.message} : ${e.error?.response?.message}`, "error", 7000);
     console.log(e.message, e.error?.response);
+  }
+
+  noDataError() {
+    this.endLoading();
+    notify(this.localizeService.localize("aucune-donnee-recuperee"), "warning", 7000);
   }
 
   endLoading() {
@@ -116,12 +149,20 @@ export class ImportProgrammesPopupComponent implements OnChanges {
     this.popupFullscreen = !this.popupFullscreen;
   }
 
+  start(e) {
+    console.log(e);
+    e.event.stopImmediatePropagation();
+  }
+
   quitPopup() {
-    if (this.loading) return;
-    confirm(
-      this.localizeService.localize("text-popup-quit"),
-      this.localizeService.localize("title-import-programme-popup").replace(" &P", "")
-    ).then(res => this.popup.visible = !res);
+    if (this.loading) {
+      confirm(
+        this.localizeService.localize("text-popup-quit"),
+        this.localizeService.localize("title-import-programme-popup").replace(" &P", "")
+      ).then(res => this.popup.visible = !res);
+    } else {
+      this.popup.visible = false;
+    }
   }
 
 }
