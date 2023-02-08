e9bfa2f694efe351d193de778f025f8b5dafa013
PascalG <pascalgautier@microtec.fr>
2023-02-08 09:53:53 +0100

    #19506 Ajout toast erreur subscribe fbonAFacturer

--- src/app/pages/ordres/form/form.component.ts
+++ src/app/pages/ordres/form/form.component.ts
@@ -756,7 +756,8 @@ export class FormComponent implements OnInit, OnDestroy, AfterViewInit {
         "fAnnulationOrdre",
         "fCreeOrdreComplementaire",
         "fCreeOrdreComplementaire",
-        "fnMajOrdreRegroupementV2"
+        "fnMajOrdreRegroupementV2",
+        "fBonAFacturer"
       ];
     functionNames.map(fn => mess = mess.replace(`Exception while fetching data (/${fn}) : `, ""));
     mess = mess.charAt(0).toUpperCase() + mess.slice(1);
@@ -1193,9 +1194,14 @@ export class FormComponent implements OnInit, OnDestroy, AfterViewInit {
 
     const societe: Societe = this.currentCompanyService.getCompany();
 
-    this.ordresBafService.fBonAFacturer([this.ordre.id], societe.id).subscribe(res => {
-      this.refreshHeader();
-    });
+    this.ordresBafService.fBonAFacturer([this.ordre.id], societe.id)
+      .subscribe({
+        error: ({ message }: Error) => {
+          console.log(message);
+          notify(this.messageFormat(message), "error", 7000);
+        },
+        next: () => this.refreshHeader()
+      });
 
   }
 
