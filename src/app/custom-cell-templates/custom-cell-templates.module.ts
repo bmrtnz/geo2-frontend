7b9e13a51103f135c5d913d6f398879d65693f97
Thomas <thomas@microtec.fr>
2023-03-21 10:34:20 +0100

    add `MergeEditSiblingColumns` cell template

--- src/app/custom-cell-templates/custom-cell-templates.module.ts
+++ src/app/custom-cell-templates/custom-cell-templates.module.ts
@@ -3,16 +3,26 @@ import { CommonModule } from "@angular/common";
 import {
   MergedDxgridCellTemplateComponent,
   MergeSiblingColumnsPipe,
+  MergeEditSiblingColumnsPipe,
 } from "./merged-dxgrid-cell-template/merged-dxgrid-cell-template.component";
-import { DxDataGridModule } from "devextreme-angular";
+import { DxDataGridModule, DxTextBoxModule } from "devextreme-angular";
 
 @NgModule({
-  declarations: [MergedDxgridCellTemplateComponent, MergeSiblingColumnsPipe],
+  declarations: [
+    MergedDxgridCellTemplateComponent,
+    MergeSiblingColumnsPipe,
+    MergeEditSiblingColumnsPipe,
+  ],
   imports: [
     CommonModule,
     DxDataGridModule,
+    DxTextBoxModule,
   ],
-  exports: [MergedDxgridCellTemplateComponent, MergeSiblingColumnsPipe],
+  exports: [
+    MergedDxgridCellTemplateComponent,
+    MergeSiblingColumnsPipe,
+    MergeEditSiblingColumnsPipe,
+  ],
   providers: [MergeSiblingColumnsPipe],
 })
 export class CustomCellTemplatesModule { }
--- src/app/custom-cell-templates/merged-dxgrid-cell-template/merged-dxgrid-cell-template.component.ts
+++ src/app/custom-cell-templates/merged-dxgrid-cell-template/merged-dxgrid-cell-template.component.ts
@@ -1,4 +1,5 @@
-import { Component, Pipe, PipeTransform } from "@angular/core";
+import { Component, Pipe, PipeTransform, SecurityContext } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 import dxDataGrid from "devextreme/ui/data_grid";
 
 @Component({
@@ -10,19 +11,44 @@ export class MergedDxgridCellTemplateComponent { }
 
 @Pipe({ name: "mergeSiblingColumns" })
 export class MergeSiblingColumnsPipe implements PipeTransform {
-  transform(input: { cellElement: HTMLElement, columnIndex: number, component: dxDataGrid, element: HTMLElement }) {
+  constructor(private domSanitizer: DomSanitizer) { }
+
+  transform(
+    input: {
+      cellElement: HTMLElement,
+      columnIndex: number,
+      rowIndex: number,
+      component: dxDataGrid,
+      element: HTMLElement,
+    }) {
 
     const htmlStructure = Object.entries({
-      client: input.cellElement.previousElementSibling,
-      responsable: input.cellElement.nextElementSibling
+      [input.columnIndex - 1]: input.cellElement.previousElementSibling,
+      [input.columnIndex + 1]: input.cellElement.nextElementSibling
     })
-      .map(([, target]) => {
+      .map(([columnIndex, target]) => {
         const containerElm = document.createElement("div");
         containerElm.innerHTML += target.innerHTML;
         return containerElm;
-      });
-
-    return htmlStructure
+      })
       .reduce((accumulator, current) => accumulator + current.outerHTML, "");
+
+    return this.domSanitizer.sanitize(SecurityContext.HTML, htmlStructure);
+  }
+}
+
+@Pipe({ name: "mergeEditSiblingColumns" })
+export class MergeEditSiblingColumnsPipe implements PipeTransform {
+  transform(
+    input: {
+      cellElement: HTMLElement,
+      columnIndex: number,
+      rowIndex: number,
+      component: dxDataGrid,
+      element: HTMLElement,
+    },
+    sibling: "previous" | "next") {
+    const columnIndex = input.columnIndex + (sibling === "previous" ? -1 : 1);
+    return input.component.cellValue(input.rowIndex, columnIndex);
   }
 }
