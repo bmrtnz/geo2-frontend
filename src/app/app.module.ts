19deb6e42c6412e8143164759962efce0907e97d
Thomas <thomas@microtec.fr>
2023-10-09 16:11:09 +0200

    add `angular/pwa` dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,6 +8,7 @@ import { GraphQLModule } from "./graphql.module";
 import { AuthService, ScreenService } from "./shared/services";
 import { OrdresIndicatorsService } from "./shared/services/ordres-indicators.service";
 import { SharedModule } from "./shared/shared.module";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +18,12 @@ import { SharedModule } from "./shared/shared.module";
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   exports: [],
   providers: [AuthService, ScreenService, DatePipe, OrdresIndicatorsService],
