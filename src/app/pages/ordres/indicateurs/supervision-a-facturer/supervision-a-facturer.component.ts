c00e5288dd52b073c161bc5e641071313b77b644
PascalG <pascalgautier@microtec.fr>
2023-11-02 16:07:28 +0100

    #23026 Indicateurs BAF Front

--- src/app/pages/ordres/indicateurs/supervision-a-facturer/supervision-a-facturer.component.ts
+++ src/app/pages/ordres/indicateurs/supervision-a-facturer/supervision-a-facturer.component.ts
@@ -432,6 +432,8 @@ export class SupervisionAFacturerComponent implements OnInit, AfterViewInit {
     const ordreRefs = this.datagrid.instance
       .getSelectedRowsData()
       .map((row: Partial<OrdreBaf>) => row.ordreRef);
+    notify(this.localization.localize("invoice-running"), "info", 5000);
+    this.datagrid.instance.beginCustomLoading("");
     this.ordresBafService
       .fBonAFacturer(
         ordreRefs,
@@ -439,7 +441,11 @@ export class SupervisionAFacturerComponent implements OnInit, AfterViewInit {
         true
       ) // true for silent mode without warnings
       .subscribe({
-        complete: () => this.enableFilters(),
+        complete: () => {
+          this.datagrid.instance.endCustomLoading();
+          notify(this.localization.localize("invoice-finished"), "success", 5000);
+          this.enableFilters();
+        }
       });
   }
 
