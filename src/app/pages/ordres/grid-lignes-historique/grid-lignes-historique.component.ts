ea661cf0374ca5a7688900491c1178c732d28c29
PascalG <pascalgautier@microtec.fr>
2023-11-27 10:13:49 +0100

    #23305 No period fix for histo ordres

--- src/app/pages/ordres/grid-lignes-historique/grid-lignes-historique.component.ts
+++ src/app/pages/ordres/grid-lignes-historique/grid-lignes-historique.component.ts
@@ -203,14 +203,18 @@ export class GridLignesHistoriqueComponent implements OnChanges, AfterViewInit {
       ["ordre.societe.id", "=", this.currentCompanyService.getCompany().id],
       "and",
       ["ordre.secteurCommercial.id", "=", values.secteur.id],
-      "and",
-      [`ordre.${dateType}`, ">=", this.dateManagementService.startOfDay(values.dateMin)],
-      "and",
-      [`ordre.${dateType}`, "<=", this.dateManagementService.endOfDay(values.dateMax)],
       // "and",
       // [[`nombreColisCommandes`, "<>", 0], "and", [`nombreColisExpedies`, "<>", 0]] // Colis cdés et exp à 0: pas d'affichage de la ligne
       // Lignes commentées 21-08-2023 suite volonté BW d'avoir accès ouvertures de calibre (0 colis exp, 0 colis cdés)
     ];
+    if (values?.dateMin) filter.push(
+      "and",
+      [`ordre.${dateType}`, ">=", this.dateManagementService.startOfDay(values.dateMin)],
+    );
+    if (values?.dateMax) filter.push(
+      "and",
+      [`ordre.${dateType}`, "<=", this.dateManagementService.endOfDay(values.dateMax)],
+    );
     if (values.client?.id) {
       filter.push("and", ["ordre.client.id", "=", values.client.id]);
     }
