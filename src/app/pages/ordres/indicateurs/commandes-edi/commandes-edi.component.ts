bb71cde2508e2de5457f5d7a291f5faf26710ba4
PascalG <pascalgautier@microtec.fr>
2024-01-30 15:31:05 +0100

    #24256 Cde Edi opening as Tab

--- src/app/pages/ordres/indicateurs/commandes-edi/commandes-edi.component.ts
+++ src/app/pages/ordres/indicateurs/commandes-edi/commandes-edi.component.ts
@@ -8,7 +8,6 @@ import { DxPopupComponent, DxScrollViewComponent } from "devextreme-angular";
 })
 export class CommandesEdiComponent {
   public visible: boolean;
-  public popupFullscreen = true;
   public loadingRecap;
 
   @ViewChild(DxScrollViewComponent, { static: false })
@@ -17,35 +16,8 @@ export class CommandesEdiComponent {
 
   constructor() { }
 
-  onShowing(e) {
-    e.component.content().parentNode.classList.add("commandes-edi-popup");
-  }
-
-  onShown(e) {
-    if (this.dxScrollView) this.dxScrollView.instance.scrollTo(0);
-  }
-
-  onHidden() {
-    this.loadingRecap = false;
-  }
-
-  hidePopup() {
-    this.popup.visible = false;
-  }
-
   showHideLoader(e) {
     this.loadingRecap = e;
-  }
-
-  resizePopup() {
-    this.popupFullscreen = !this.popupFullscreen;
-  }
-
-  capitalize(data) {
-    return data?.description
-      ? data.description.charAt(0).toUpperCase() +
-      data.description.slice(1).toLowerCase()
-      : null;
   }
 }
 
