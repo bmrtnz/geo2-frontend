2952c7d3fe8c1d68ff2c87fe0d153fc4bdd467ba
PascalG <pascalgautier@microtec.fr>
2024-02-01 10:03:04 +0100

    #24252 Modif group text plann transp

--- src/app/pages/ordres/indicateurs/planning-transporteurs/planning-transporteurs.component.ts
+++ src/app/pages/ordres/indicateurs/planning-transporteurs/planning-transporteurs.component.ts
@@ -280,11 +280,7 @@ export class PlanningTransporteursComponent implements OnInit, AfterViewInit {
 
   calculateEntrepot(data) {
     // Ajout CP, ville et pays au lieu de livraison
-    let title = `(Transp. : ${data.transporteur})`;
-    if (data.entrepotCodePostal) {
-      title += `  ${data?.entrepotRaisonSocial} - ${data.entrepotCodePostal} ${data.entrepotVille} (${data.entrepotPays})`;
-    }
-    return title;
+    return `${data?.entrepotRaisonSocial} - ${data.entrepotCodePostal ?? ''} ${data.entrepotVille} (${data.entrepotPays})`;
   }
 
   onValideChanged(e) {
